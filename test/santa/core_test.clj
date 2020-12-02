54c7da28726f1be1f2cc8695f6eed441ae2df370
Darko Prelec <darko.prelec@gmail.com>
2020-12-02 21:10:55 +0100

    Added code for day2

--- test/santa/core_test.clj
+++ test/santa/core_test.clj
@@ -15,3 +15,18 @@
   (testing "Testing problem 1-2."
     (is (= (day-1-2 test-input-1) test-output-2))))
 
+(def test-input-2-1 
+  [["1-3" "a:" "abcde"]
+   ["1-3" "b:" "cdefg"]
+   ["2-9" "c:" "ccccccccc"]])
+
+(deftest test-parse-expect
+  (testing "parse-expect"
+    (is (= (parse-expect "1-2") '(1 2)))))
+
+(def test-problem-2-1
+  (is (= (count-valid-password-by-letter-count test-input-2-1) 2)))
+
+(def test-problem-2-2
+  (is (= (count-valid-password-by-letter-position test-problem-2-1) 1)))
+
