4ffbf8ee846673b206d8ff20b4ed6b3b323a3365
Darko Prelec <darko.prelec@gmail.com>
2020-12-10 20:33:54 +0100

    day8: converted program into vec

--- src/santa/2020/day8/day8.clj
+++ src/santa/2020/day8/day8.clj
@@ -6,18 +6,18 @@
   {0 [nop +0], 1 [acc +1], 2 [jmp +4] ..}."
   [input-file]
   (let [lines (split-lines (slurp input-file))]
-    (loop [lines lines acc {} i 0]
+    (loop [lines lines acc [] i 0]
       (if-not (seq lines)
         acc
         (let [line (first lines) part (split line #" ")]
-          (recur (rest lines) (assoc acc i part) (inc i)))))))
+          (recur (rest lines) (conj acc part) (inc i)))))))
 
 (defn execute-program 
   "Executes a map of instructions. Stops when pointer value repeats for the
   second time."
   [prog]
   (loop [ptr 0 acc 0 seen #{}]
-    (let [instr (prog ptr)]
+    (let [instr (nth prog ptr)]
       (if (or (nil? instr) (contains? seen ptr))
         acc
         (let [[op i] instr nxt (read-string i)]
