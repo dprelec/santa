7b4eb4eba8d32525b8da657bb3483caae97679dd
Darko Prelec <darko.prelec@epti.com>
2022-12-05 08:04:27 +0100

    add 2022 day5

--- src/santa/2015/day13/day13.clj
+++ src/santa/2015/day13/day13.clj
@@ -18,8 +18,7 @@
                              ((scores candidate) member))})
         sorted (sort-by :score matches)
         sorted (reverse sorted)]
-    ;(println sorted)
-    [(first sorted) (second sorted)]))
+    sorted))
 
 (defn solve-1 [input-file]
   (let [input (input->vec input-file)
@@ -33,33 +32,10 @@
         matches (map #(score-pairs scores members %) members)
         matches (apply concat matches)
         matches (distinct matches)
-        scores (map :score matches)]
-    (doseq [s matches]
-      (println "m:" (:key s)))
-    (apply + scores)))
-
-;; scorese merged
-(def test-data
-  {"Alice" {"Bob" 54, "Carol" -79, "David" -2},
-   "Bob" {"Alice" 83, "Carol" -7, "David" -63},
-   "Carol" {"Alice" -62, "Bob" 60, "David" 55},
-   "David" {"Alice" 46, "Bob" -7, "Carol" 41}})
-
-;; scoremap
-(def test-data-2
-  '({Alice {Bob 54}}
-    {Alice {Carol -79}}
-    {Alice {David -2}}
-    {Bob {Alice 83}}
-    {Bob {Carol -7}}
-    {Bob {David -63}}
-    {Carol {Alice -62}}
-    {Carol {Bob 60}}
-    {Carol {David 55}}
-    {David {Alice 46}}
-    {David {Bob -7}}
-    {David {Carol 41}}))
-
+        sorted-all (reverse (sort-by :score matches))]
+    (doseq [s sorted-all]
+      (println "m:" s))    
+      sorted-all))
 
 (println (solve-1 "test-input"))
 
---
+++ src/santa/2022/day5/day5.clj
@@ -0,0 +1,92 @@
+(ns day5 (:require [clojure.string :refer [join split split-lines]]))
+
+(defn input->vec [input-file] (split-lines (slurp input-file)))
+
+(defn transpose [matrix] (apply mapv vector matrix))
+
+(defn crate? [line]
+  (let [line (remove #(= \[ %) line)
+        line (remove #(= \] %) line)
+        line (remove #(= \space %) line)]
+    (> (count line) 0)))
+
+(defn space? [c] (= c \space))
+
+(defn top [crates]
+  (into [] (for [k (sort (keys crates))]
+             (last (crates k)))))
+
+(defn solution-part-1 [crates moves]
+    (loop [moves moves crates crates]
+      (if-not (seq moves)
+        (join "" (top crates))
+        (let [;; parse move lines
+              move (split (first moves) #" ")
+              ;; amount is integer
+              amount (read-string (nth move 1))
+              ;; from and to are string keys of crates
+              from (nth move 3)
+              to (nth move 5)
+              ;; source crates to be moved
+              payload (take amount (reverse (crates from)))
+              new-crate (concat (crates to) payload)
+              old-crate (take (- (count (crates from)) amount) (crates from))
+              ;; modify crates 
+              crates (assoc crates to new-crate)
+              crates (assoc crates from old-crate)]
+          (recur (rest moves) crates)))))
+
+(defn solution-part-2 [crates moves]
+    (loop [moves moves crates crates]
+      (if-not (seq moves)
+        (join "" (top crates))
+        (let [;; parse move lines
+              move (split (first moves) #" ")
+              ;; amount is integer
+              amount (read-string (nth move 1))
+              ;; from and to are string keys of crates
+              from (nth move 3)
+              to (nth move 5)
+              ;; source crates to be moved
+              ;; double reverse to preserve order!
+              payload (take amount (reverse (crates from)))
+              new-crate (concat (crates to) (reverse payload))
+              old-crate (take (- (count (crates from)) amount) (crates from))
+              ;; modify crates 
+              crates (assoc crates to new-crate)
+              crates (assoc crates from old-crate)]
+          (recur (rest moves) crates)))))
+
+(defn parse-crate-moves [input]
+  (let [;; split config in two parts
+        config (partition-by #(= "" %) input)
+        ;; rotate crates config
+        crates (map reverse (transpose (first config)))
+        ;; remove non-crate lines
+        crates (filter crate? crates)
+        ;; clean-up crate lines
+        crates (map #(remove space? %) crates)
+        ;; convert to map {"crate_numer" [stack]}
+        crates (merge (into {} (for [crate crates] {(str (first crate)) (rest crate)})))
+        moves (first (rest (rest config)))]
+    [crates moves]))
+
+(defn solve-1 [input-file]
+  (let [input (input->vec input-file)
+        config (parse-crate-moves input)
+        crates (first config)
+        moves (second config)]
+    (solution-part-1 crates moves)))
+
+(println "T1:" (solve-1 "test-input"))
+(println "P1:" (solve-1 "input"))
+
+(defn solve-2 [input-file]
+  (let [input (input->vec input-file)
+        config (parse-crate-moves input)
+        crates (first config)
+        moves (second config)]
+    (solution-part-2 crates moves)))
+
+(println "T2:" (solve-2 "test-input"))
+(println "P2:" (solve-2 "input"))
